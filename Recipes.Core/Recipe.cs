89b999d05734ac88d43b34d057fe7eb61b4d4871
Mohamed Khalifa <muhamed.khalifah@gmail.com>
2020-01-22 13:30:52 +0200

    adding Recipes.Core project

---
+++ Recipes.Core/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipes.Core
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Review { get; set; }
+        public RecipeCategory Category { get; set; }
+    }
+}
