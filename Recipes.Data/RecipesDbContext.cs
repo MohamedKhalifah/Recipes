8985f908f52c42d58bad03a40bd2173a6b357f7f
Mohamed Khalifa <muhamed.khalifah@gmail.com>
2020-01-22 21:33:08 +0200

    adding entityframeworkcore, add and run migrations

---
+++ Recipes.Data/RecipesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipes.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipes.Data
+{
+    public class RecipesDbContext : DbContext
+    {
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
