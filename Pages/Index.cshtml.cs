a93c688f5cb27531fa8b4b18fb51c428f5de3663
Mohamed Khalifa <muhamed.khalifah@gmail.com>
2020-01-22 13:34:28 +0200

    adding configured welcome message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Recipes.Pages
@@ -11,15 +12,18 @@ namespace Recipes.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public string WelcomeMessage { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            WelcomeMessage = _configuration["WelcomeMessage"];
         }
     }
 }
