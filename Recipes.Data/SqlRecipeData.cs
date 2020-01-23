4dacd813ee693a46a8a7265297897006cc227b29
Mohamed Khalifa <muhamed.khalifah@gmail.com>
2020-01-23 17:06:32 +0200

    adding database service

---
+++ Recipes.Data/SqlRecipeData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Core;
+
+namespace Recipes.Data
+{
+    public class SqlRecipeData : IRecipeData
+    {
+        private readonly RecipesDbContext dbContext;
+
+        public SqlRecipeData(RecipesDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public int Commit()
+        {
+            int result = dbContext.SaveChanges();
+            return result;
+        }
+
+        public void CreateRecipe(Recipe recipe)
+        {
+            dbContext.Recipes.Add(recipe);
+        }
+
+        public void DeleteRecipe(int id)
+        {
+            var recipe = GetRecipeById(id);
+            if (recipe == null)
+            {
+                return;
+            }
+            dbContext.Recipes.Remove(recipe);
+        }
+
+        public IEnumerable<Recipe> GetAll()
+        {
+            return dbContext.Recipes.ToList();
+        }
+
+        public Recipe GetRecipeById(int id)
+        {
+            return dbContext.Recipes.Find(id);
+        }
+
+        public IEnumerable<Recipe> GetRecipesByName(string name)
+        {
+            return dbContext.Recipes.Where(r => r.Name.StartsWith(name) ||
+                                                string.IsNullOrEmpty(name))
+                                                .OrderBy(r => r.Name);
+        }
+
+        public Recipe UpdateRecipe(Recipe updatedRecipe)
+        {
+            var entity = dbContext.Recipes.Attach(updatedRecipe);
+            entity.State = EntityState.Modified;
+            return updatedRecipe;
+        }
+    }
+}
