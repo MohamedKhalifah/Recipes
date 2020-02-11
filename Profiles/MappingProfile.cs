c8066b41f78a3cfcc2ff827d1a5ef3f48169908e
MohamedKhalifah <37842983+MohamedKhalifah@users.noreply.github.com>
2020-02-11 20:32:30 +0200

    adding automapper and mappings profile

---
+++ Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Recipes.Core;
+using Recipes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipes.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Recipe, RecipeModel>().ReverseMap();
+        }
+    }
+}
